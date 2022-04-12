60d81845d50e1b158a9b4af740102c9582a8a5c5
Yune Su Nadi <yunesunadi.dev@gmail.com>
2022-04-12 21:26:59 +0630

    finished project

---
+++ Sidebar/app.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector(".close-icon");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-item .nav-link");
+
+let toggleMenu = (btn) => {
+    btn.addEventListener("click", () => {
+        nav.classList.toggle("show-menu");
+        navLinks.forEach((navLink, index) => {
+            navLink.classList.toggle("show-animation");
+            if (navLink.style.animationDelay) {
+                navLink.style.animationDelay = "";
+            } else {
+                navLink.style.animationDelay = `${index * 0.3}s`;
+            }
+        });
+    });
+};
+
+toggleMenu(menuBtn);
+toggleMenu(closeBtn);
